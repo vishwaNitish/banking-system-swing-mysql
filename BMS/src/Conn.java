5866c199e6fc078884bd3d73e1f7aab5cc42f691
vishwaNitish <155847058+vishwaNitish@users.noreply.github.com>
2025-08-21 10:21:08 +0530

    Update Conn.java

--- BMS/src/Conn.java
+++ BMS/src/Conn.java
@@ -15,12 +15,13 @@ public class Conn {
   public Conn() {
     try {
       Class.forName("com.mysql.cj.jdbc.Driver");
-      c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "Shilpa@8187");
+      c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "");
       s = c.createStatement();
 
     } catch (Exception e) {
       System.out.println(e);
     }
   }
+
 
 }
